8ac9426ab55cd1eaaed3e413d64216e557165151
Altleri <taimisneli@gmail.com>
2025-08-15 17:58:17 +0300

    Update Project_JS_Anastasia_Altshuler_and_Evgeny_Altshuler.js
    *Setting of popup. Take elements of popup from HTML
    *Create popup that show current information about the contact
    *Create Function that get to edit contact information
    *Created function for adding new contact to the list
    *Added event click that close all popups
    *Create function for cleare input area

--- Project_JS_Anastasia_Altshuler_and_Evgeny_Altshuler.js
+++ Project_JS_Anastasia_Altshuler_and_Evgeny_Altshuler.js
@@ -168,3 +168,177 @@ function deleteContact(){
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+//Setting of popup. Take elements of popup from HTML
+const generalPopup = document.getElementById('generalPopup');
+const contactName = document.querySelector('.popup-user-name');
+const contactPhone = document.querySelector('.popup-user-phone');
+const contactAddress = document.querySelector('.popup-user-address');
+const contactAge = document.querySelector('.popup-user-age');
+const contactImageURL = document.querySelector('.popup-user-image');
+
+//Create popup that show current information about the contact
+function showInfoContact(){
+   //Adding event click on ul area 
+  contactsListElement.addEventListener('click', (e) =>{
+      //Checking if area where target the user is a element with class info-img
+      if (e.target.classList.contains('info-img')) {
+        //if yes we save container what located near the target area
+        const contactEL = e.target.closest('.user-items');
+        //And also save attribute of this contact
+        const contactID = +contactEL.getAttribute('data-id');
+        //Created new variable that take value of object from array with same ID as contactID
+        const elem = contacts.find(el => el.id == contactID)
+      
+        //Popup lines take the value of the contact the user clicked on
+        document.querySelector('.current-user-name').textContent = elem.name;
+        document.querySelector('.current-user-phone').textContent = elem.phone;
+        document.querySelector('.current-user-age').textContent = elem.age; 
+        document.querySelector('.current-user-address').textContent = elem.address;
+        
+        //Display the main popup
+        generalPopup.style.display = 'block';
+
+        //display the popup body. In this case is info popup
+        document.querySelector('.info-form').style.display='block';
+        }
+      })
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Function that get to edit contact information
+function editContact(){
+   //Adding event click on ul area 
+  contactsListElement.addEventListener('click', (e) =>{
+      //Checking if area where target the user is a element with class edit-img
+      if(e.target.classList.contains('edit-img'))
+          {    //if yes we save container what located near the target area
+              const contactEL = e.target.closest('.user-items');
+              //And also save attribute of this contact
+              const contactID = +contactEL.getAttribute('data-id');
+              //Created new variable that take value of object from array with same ID as contactID
+              const elem = contacts.find(el => el.id == contactID )
+
+              //Created new value that take elements from html.
+              const editForm = document.querySelector('.edit-form');
+              const saveBotton = editForm.querySelector('.save-botton');
+
+              //Show the popup
+              generalPopup.style.display ='block';
+              editForm.style.display='block';
+
+              //Open popup will be in personal data of the cintact in inputs
+              editForm.querySelector('.popup-user-name').value = elem.name;
+              editForm.querySelector('.popup-user-phone').value = elem.phone;
+              editForm.querySelector('.popup-user-age').value = elem.age;
+              editForm.querySelector('.popup-user-address').value = elem.address;
+              editForm.querySelector('.popup-user-image').value = elem.imageUrl;
+
+              
+                      //Added event on click
+                      saveBotton.addEventListener('click', (e) => {
+                      e.preventDefault();//for inputs
+                      //Conacts that we choosed in function that opened edit popup
+                      //Saved new provided information 
+                      elem.name = editForm.querySelector('.popup-user-name').value;
+                      elem.phone = editForm.querySelector('.popup-user-phone').value;
+                      elem.address = editForm.querySelector('.popup-user-address').value;
+                      elem.age = editForm.querySelector('.popup-user-age').value;
+                      elem.imageUrl = editForm.querySelector('.popup-user-image').value;
+                      //Message
+                      alert("Saved")
+                      //Call two functions. Clean inputs liners from data and show contact to update showing information
+                      cleanTheInputFormArea();
+                      showContactsList();
+                    })
+                  }
+         })
+      }
+////////////////////////////////////////////////
+      //Call function 
+      deleteContact();
+      showInfoContact();
+      editContact();
+////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Take element from html. It's add img
+const addNewContact = document.getElementById('add-Person')
+
+//Added event on click on this element
+addNewContact.addEventListener('click', () => {
+
+  //Created new value that take value of html element
+  const createForm = document.querySelector('.create-form')
+  //Save botton from create-form
+  const createSaveBotton = createForm.querySelector('.save-botton');
+
+  //Show general popup, and form for creating contact 
+  generalPopup.style.display ='block';
+  createForm.style.display='block';
+
+   //Created function
+    const addSendData = (e) =>{
+      e.preventDefault();//for inputs form
+
+      //Created new variable that will take new value from inputs of creatForm
+      const newContactName = createForm.querySelector('.popup-user-name').value;
+      const newContactPhone = createForm.querySelector('.popup-user-phone').value;
+      const newContactAddress = createForm.querySelector('.popup-user-address').value;
+      const newContactAge = createForm.querySelector('.popup-user-age').value;
+      const newContactImage = createForm.querySelector('.popup-user-image').value;
+
+      //Check if inputs they are not empty
+      if (!newContactName.trim() || !newContactPhone.trim() || !newContactAddress.trim() || !newContactAge.trim() || !newContactImage.trim()) {
+        alert("All fields required!")//if one of them empty, show this message
+      } else {
+        //Of all good, create new object this provided data
+        const newObj = {
+            id: contacts.length + 1,
+            name: newContactName,
+            address: newContactAddress,
+            age: newContactAge,
+            phone: newContactPhone,
+            imageUrl: newContactImage
+    }
+            //Add new object to our array
+            contacts.push(newObj);
+            alert("DONE")//Show message
+
+            //Close popups
+            generalPopup.style.display = 'none'
+            createForm.style.display ='none';
+
+            //call function that cleane the inputs area
+            cleanTheInputFormArea();
+            //call show contact for update our contact list
+            showContactsList();
+    }
+}//close thw function addSendData
+
+//Added event click to our save botton and call the function addSendData
+createSaveBotton.addEventListener('click',addSendData);
+})
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Get X that close the popup
+const closeGeneralPopup = document.getElementById('closeModal');
+//The event on click on close popup botton. This click will close all opened popups
+  closeGeneralPopup.addEventListener('click', () => {
+    generalPopup.style.display = 'none';
+    document.querySelector('.info-form').style.display ='none';
+    document.querySelector('.create-form').style.display ='none';
+    document.querySelector('.edit-form').style.display ='none';
+  })
+
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Create function for cleare input area
+function cleanTheInputFormArea()
+{
+  contactName.value="";
+  contactPhone.value="";
+  contactAddress.value="";
+  contactAge.value="";
+  contactImageURL.value="";
+}
+
+

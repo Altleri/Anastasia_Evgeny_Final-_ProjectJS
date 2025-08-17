f6aaa4d7e44b2e697f8258410caffc028770b6d6
Altleri <taimisneli@gmail.com>
2025-08-17 19:29:26 +0300

    Update index.js
    general fixing

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,14 @@
 "use strict";//Don't forget to add "use strict"
 
 showContactsList();//Call the function that upload all element(contacts) to the page
-removeAllContacts();
-searchInputContacts();
-deleteContact();
-showInfoContact();
-editContact();
-addNewContactToArray();
-hidePopupByClickOnClose();
-userAreaColorChanging();
-darkThemAnimation();
+removeAllContacts();//Call function that removed all contacts on the page
+searchInputContacts();//Call function that search contact according yo input value
+deleteContact();//Call function that delete any specified contact
+showInfoContact();//Call function that show information about contact
+editContact();//Call function that allows you to edit a contact
+addNewContactToArray();//Call function that created new contact
+hidePopupByClickOnClose();//Call function that close all popup window by click on X
+userAreaColorChanging();//Call function that changing color area when user mouseon or mouseout cursor
+darkThemAnimation();//Call function that changing theme of the page to dark theme
+textAppearIfContactsLengthIs0();//Check contacts length
+
